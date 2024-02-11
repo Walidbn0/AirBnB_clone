ff113a0f6ec38eb51bb0062af6d2443dcd7bf00c
simodevjs <simodevjs@gmail.com>
2024-02-11 20:33:39 +0100

    simo

--- models/__init__.py
+++ models/__init__.py
@@ -2,8 +2,7 @@
 """Creates a unique FileStorage instance for the application"""
 
 from models.engine.file_storage import FileStorage
-from .base_model import BaseModel
-from .user import User  # Ensure User is imported
+
 
 storage = FileStorage()
 storage.reload()
--- models/amenity.py
+++ models/amenity.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from models.base_models import BaseModel
+from models.base_model import BaseModel
 
 class Amenity(BaseModel):
     """A class that represents a amenity
