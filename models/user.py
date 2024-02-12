e3739122ae947273a7f7dd0a18ff3ac3a5c2764f
Walidbn0 <waliidbenhammii@gmail.com>
2024-02-12 13:10:26 -0800

    AirBnb HBNB

--- models/user.py
+++ models/user.py
@@ -15,3 +15,7 @@ class User(BaseModel):
     password = ""
     first_name = ""
     last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        '''Initializes attributes for the User class'''
+        super().__init__(*args, **kwargs)
